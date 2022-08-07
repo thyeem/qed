a83b84616bd89ba28222448a67befb7b964d1f1a
Francis Lim <thyeem@gmail.com>
2022-08-07 11:20:34 +0900

    added test cases passing data via args/stdin

--- test.sh
+++ test.sh
@@ -32,12 +32,20 @@ error () {
     exit 1
 }
 
+when_encode_decode_with_data_passed_via_stdin () {
+    cat ${files[5]} | ${QED} -e | ${QED} -d
+}
+
+when_encode_decode_with_data_passed_via_args () {
+    local files=${files[@]:0:3}
+    ${QED} -e ${files[@]} | ${QED} -d
+}
 
 when_encoded_files_are_renamed_mixed_duplicated () {
     local files=${files[@]:5:5}
     local out=`${QED} -e ${files[@]}`
 
-    # randomizes renames, and duplicate files encoded.
+    # randomizes renames, and duplicates files encoded.
     pushd . &> /dev/null
     cd $out
 
@@ -53,14 +61,13 @@ when_encoded_files_are_renamed_mixed_duplicated () {
     done
     popd &> /dev/null
 
-
     # try to decode. let's go back to before!
     ${QED} -d $out
 }
 
 
 when_provided_with_option_images_on_browser () {
-    ${QED} -eq ${files[5]}
+    ${QED} -eq ${files[5]} | ${QED} -d
 }
 
 
@@ -69,6 +76,9 @@ when_encode_decode_with_multiple_file_types () {
 }
 
 
+when_encode_decode_with_data_passed_via_stdin
+
+when_encode_decode_with_data_passed_via_args
 
 when_encoded_files_are_renamed_mixed_duplicated 2
 
